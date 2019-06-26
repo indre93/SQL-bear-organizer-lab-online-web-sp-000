18dcbc17ed459b86eb9ce94b34212968fb6f4fbd
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 02:32:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,9 +18,9 @@ def select_youngest_bear_and_returns_name_and_age
   "SELECT name, MIN(age) FROM bears"
 end
 
-# selects the most prominent color and returns it with its count
+# selects the most prominent color and returns it with its count ["dark brown", 3]
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color)"
+  "SELECT color, COUNT(color) FROM bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
