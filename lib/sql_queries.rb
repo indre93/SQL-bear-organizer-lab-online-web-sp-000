49c881a1cb4138d965530aefdeb15504c9920106
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 02:43:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 # selects the most prominent color and returns it with its count ["dark brown", 3]
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color) FROM bears GROUP BY HAVING COUNT(color)"
+  "SELECT MAX(color) FROM bears ORDER BY COUNT(color)"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
