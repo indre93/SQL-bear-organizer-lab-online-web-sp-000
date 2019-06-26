c22009b505c59cd263c537aeda69cf91ae3a4b17
Indre Bakeer <indre.bakeer@gmail.com>
2019-06-26 02:14:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,12 +18,13 @@ def select_youngest_bear_and_returns_name_and_age
   "SELECT name, MIN(age) FROM bears"
 end
 
+#["dark brown", 3]
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT count(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
